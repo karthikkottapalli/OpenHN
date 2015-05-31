cdf8727f42d0fe54b2196b5ab431dcfbe585ec22
Karthik Kottapalli <kartik.kottapalli@gmail.com>
2015-05-31 22:02:20 +0530

    UI changes. Upvote button and devise error fix

--- app/controllers/frontpages_controller.rb
+++ app/controllers/frontpages_controller.rb
@@ -1,4 +1,5 @@
 class FrontpagesController < ApplicationController
+  before_filter :authenticate_user! 
   before_action :set_frontpage, only: [:show, :edit, :update, :destroy]
 
   # GET /frontpages
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :frontpages do 
     resources :comments
   end
